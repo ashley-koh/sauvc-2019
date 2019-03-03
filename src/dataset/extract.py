749be1a0dc6bbcb78660d111855f91bfb27f313c
Zhu Zhan Yan <program.nom@gmail.com>
2019-03-03 14:40:38 +0800

    Added sampling rate to extract_frames frames to only extract part of the video

--- src/dataset/extract.py
+++ src/dataset/extract.py
@@ -9,13 +9,16 @@ import cv2
 import argparse
 from multiprocessing import Pool, cpu_count
 from glob import glob
+from shutil import rmtree
 
 # Extract the frames of the video specified by the given video_filename into 
 # the given directory out_dir.
 # Writes frames in the path <out_dir>/<video_name>_<frame_index>.jpg 
 # note if frame at path already exists will overwrite frame
 # if verbose is specified as true wlll print progress infomation
-def extract_frames(video_filename, out_dir, verbose=True):
+# sampling rate specifies ratio of the no of frames extracted to the total number 
+# of frames
+def extract_frames(video_filename, out_dir, sampling_rate, verbose=True):
     # Extract video name frm full filename
     video_basename = os.path.basename(video_filename)
     video_name = video_basename[:video_filename.index(".")]
@@ -25,23 +28,33 @@ def extract_frames(video_filename, out_dir, verbose=True):
     # check if video_capture is opened correctly
     if video_capture is None or not video_capture.isOpened():
         raise ValueError("Could not open video file at path: " + video_filename)
+
+    # Determine the interval in which to sample frames
     # obtain number of frames in video
-    n_frames = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    total_frames = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    n_frames = int(total_frames * sampling_rate) # no of frames to sample
+    n_interval = int(total_frames / n_frames) # no of frame to process per sampling step
+    if verbose: print("to read {} frames".format(n_frames))
                    
     # Setup output directory
     if not os.path.exists(out_dir): os.mkdir(out_dir)
     
     # Keep extracting frames until read failure
-    for i_frame in range(n_frames):
-        read_sucessful, frame = video_capture.read()
+    current_frame = 0
+    for i_frame in range(0, total_frames, n_interval):
+        # read frames, skiping frames not included in the smaple
+        while current_frame <= i_frame:
+            read_sucessful, frame = video_capture.read()
+            current_frame += 1
         if not read_sucessful:
             raise ValueError("FATAL to read frame: {} ".format(i_frame))
+
         # write frame in format specified in function  docs
         cv2.imwrite("{}/{}__{}.jpg".format(out_dir, video_name, i_frame), frame)
     
         if verbose:
             # display progress infomation
-            progress = i_frame / n_frames * 100 # compute progress percentage
+            progress = i_frame / total_frames * 100 # compute progress percentage
             print("extracted", video_name, " progress: {:.2f}%".format(progress))
 
 if __name__ == "__main__":
@@ -50,10 +63,10 @@ if __name__ == "__main__":
     parser.add_argument('-s', help='The filename of the video to extract frames from')
     options = parser.parse_args()
 
-
     ## Perform extraction 
     # Create pictures directory
     pics_dir = "data/images"
+    if os.path.exists(pics_dir): rmtree(pics_dir)
     if not os.path.exists(pics_dir): os.mkdir(pics_dir)
     
     # perform extraction on multiple processes
@@ -61,9 +74,10 @@ if __name__ == "__main__":
         i_vid, vid_path = vid_info
         print("extractor: prcoessing video: ", i_vid)
         data_dir, video_dir, class_name, video_filename = vid_path.split("/")
-        extract_frames(vid_path, out_dir="{}/{}".format(pics_dir, class_name))
+        extract_frames(vid_path, out_dir="{}/{}".format(pics_dir, class_name),
+                       sampling_rate=0.15)
 
-    vid_infos = list(enumerate(glob("data/video/*/*")))[:1]
+    vid_infos = list(enumerate(glob("data/video/*/*")))
     print("to extract {} videos".format(len(vid_infos)))
     
     pool = Pool(processes=cpu_count())
